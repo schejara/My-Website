42409db3c01e1cad0555dfd8d6254710ca5cdedc
Shobha Chejara <shobha.chejara@gmail.com>
2024-12-17 14:06:25 -0600

    Set up basic website structure

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
+
 import './Nav.css';
-import { useSelector } from 'react-redux';
+
 
 function Nav() {
-  const user = useSelector((store) => store.user);
+  
 
   return (
     <div className="nav">
@@ -13,28 +13,23 @@ function Nav() {
         <h2 className="nav-title">Prime Solo Project</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
+       
+        
           <>
-            <Link className="navLink" to="/user">
+            <Link className="navLink" to="/Home">
               Home
             </Link>
 
-            <Link className="navLink" to="/info">
+            <Link className="navLink" to="/Info">
               Info Page
             </Link>
+            <Link className="navLink" to="/LandingPage">
+              LandingPage
+            </Link>
 
-            <LogOutButton className="navLink" />
+            
           </>
-        )}
+       
 
         <Link className="navLink" to="/about">
           About
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-
-import store from './redux/store';
-
 import App from './components/App/App';
 
-
+// Render the application without Redux
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
